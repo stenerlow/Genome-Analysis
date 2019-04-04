ad648a779476e780084088941c6bee2981e05978
Oskar Stenerlöv <osst6659@r330.uppmax.uu.se>
2019-04-04 16:21:17 +0200

    Style touches

--- canu_assembly_sbatch_script.sh
+++ canu_assembly_sbatch_script.sh
@@ -3,7 +3,7 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 18:00:00
+#SBATCH -t 20:00:00
 #SBATCH -J paper_5_pacbio_assembly_oskar_stenerlov
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user oskar.stenerlow@gmail.com
@@ -13,5 +13,5 @@ module load bioinfo-tools
 module load canu/1.7
 
 #My commands
-canu -p scaffold_10_pacbio_assembly -d /home/osst6659/genome_analyses/analyses/01_pacbio_assembly genomeSize=36M -pacbio-raw /home/osst6659/genome_analyses/data/raw_data/pacbio/SRR6037732_scaffold_10.fq.gz
+canu -p 2019_04_04_scaffold_10_pacbio_assembly -d /home/osst6659/genome_analyses/analyses/01_pacbio_assembly genomeSize=36M -pacbio-raw /home/osst6659/genome_analyses/data/raw_data/pacbio/SRR6037732_scaffold_10.fq.gz
 
