11a434272038819dbd74a8892a92d285e3c8ddf4
stenerlow <oskar.stenerlow@gmail.com>
2019-04-11 14:28:41 +0200

    Added batch script for Pilon improvement

---
+++ batch_scripts/pilon_improvement.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 10
+#SBATCH -t 00:30:00
+#SBATCH -J pilon_improvement_oskar_stenerlov
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user oskar.stenerlow@gmail.com
+
+#Load modules
+module load bioinfo-tools
+module load Pilon/1.22
+
+#My commands
+pilon --genome /home/osst6659/genome_analyses/analyses/02_pacbio_illumina_alignment/2019_04_04_scaffold_10_pacbio_assembly.contigs.fasta --bam alignment_bwa.sorted.bam --output pilon_improved_assembly --outdir /home/osst6659/genome_analyses/analyses/02_pacbio_illumina_alignment/Pilon_improvement/ --diploid --threads 10
+
