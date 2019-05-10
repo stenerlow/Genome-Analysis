19e25bbb3c9a377d28e6afa738d955ff2b11e0d2
stenerlow <oskar.stenerlow@gmail.com>
2019-05-10 13:03:12 +0200

    Added super script to fix all

--- batch_scripts/trinity.sh
+++ batch_scripts/trinity.sh
@@ -2,8 +2,8 @@
 
 #SBATCH -A g2019003
 #SBATCH -p core
-#SBATCH -n 6
-#SBATCH -t 10:00:00
+#SBATCH -n 4
+#SBATCH -t 6:00:00
 #SBATCH -J paper_5_trinity
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user oskar.stenerlow@gmail.com
@@ -12,6 +12,6 @@
 module load bioinfo-tools
 module load trinity
 
-Trinity --genome_guided_bam /home/osst6659/genome_analyses/analyses/04_transcriptome_assembly/merged_sorted_tophat.bam -genome_guided_max_intron 10000 --max_memory 20G --CPU 6 --output /home/osst6659/genome_analyses/analyses/07_trinity_assembly/trinity_output --full_cleanup
+Trinity --genome_guided_bam /home/osst6659/genome_analyses/analyses/04_transcriptome_assembly/merged_sorted_tophat.bam -genome_guided_max_intron 10000 --max_memory 20G --CPU 4 --output /home/osst6659/genome_analyses/analyses/07_trinity_assembly/trinity_output
 
 
