4c5d9098dbb5f65619e5c0b1626ef3b91eef2101
stenerlow <oskar.stenerlow@gmail.com>
2019-05-23 23:14:34 +0200

    Added all the different RNA types to script

--- batch_scripts/htseq_1.sh
+++ batch_scripts/htseq_1.sh
@@ -4,12 +4,17 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 10:00:00
-#SBATCH -J paper_5_maker_1
+#SBATCH -J paper_5_diff_exp
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user oskar.stenerlow@gmail.com
 
 #Load modules
 module load bioinfo-tools
-module load maker
+module load htseq
 
-htseq-count /home/osst6659/genome_analyses/analyses/10_references_ncbi/SCAFFOLD_0097/accepted_hits97_sorted.sam
+cd /home/osst6659/genome_analyses/analyses/10_references_ncbi/
+
+htseq-count -f bam -i ID /home/osst6659/genome_analyses/analyses/10_references_ncbi/SCAFFOLD_0097/accepted_hits92_sorted.bam \
+/home/osst6659/genome_analyses/analyses/10_references_ncbi/SCAFFOLD_0097/accepted_hits93_sorted.bam \
+/home/osst6659/genome_analyses/analyses/10_references_ncbi/SCAFFOLD_0097/accepted_hits96_sorted.bam \
+/home/osst6659/genome_analyses/analyses/10_references_ncbi/SCAFFOLD_0097/accepted_hits97_sorted.bam /home/osst6659/genome_analyses/analyses/10_references_ncbi/ref_genome_annotation.gff3
