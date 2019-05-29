9c159cb3795854517e78c11f3203f07b997426e8
stenerlow <oskar.stenerlow@gmail.com>
2019-05-29 13:13:29 +0200

    Removed errors in paths

--- batch_scripts/htseq_1.sh
+++ batch_scripts/htseq_1.sh
@@ -3,7 +3,7 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 10:00:00
+#SBATCH -t 2:00:00
 #SBATCH -J paper_5_diff_exp
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user oskar.stenerlow@gmail.com
@@ -13,8 +13,7 @@ module load bioinfo-tools
 module load htseq
 
 cd /home/osst6659/genome_analyses/analyses/10_references_ncbi/
-
-htseq-count -f bam -i ID /home/osst6659/genome_analyses/analyses/10_references_ncbi/SCAFFOLD_0097/accepted_hits92_sorted.bam \
-/home/osst6659/genome_analyses/analyses/10_references_ncbi/SCAFFOLD_0097/accepted_hits93_sorted.bam \
-/home/osst6659/genome_analyses/analyses/10_references_ncbi/SCAFFOLD_0097/accepted_hits96_sorted.bam \
-/home/osst6659/genome_analyses/analyses/10_references_ncbi/SCAFFOLD_0097/accepted_hits97_sorted.bam /home/osst6659/genome_analyses/analyses/10_references_ncbi/ref_genome_annotation.gff3
+htseq-count --format='bam' --order='name' --stranded='yes' --type='CDS' --idattr='gene_id' --samout='counts_92.sam' /home/osst6659/genome_analyses/analyses/10_references_ncbi/SCAFFOLD_0092/accepted_hits92_sorted.bam /home/osst6659/genome_analyses/analyses/10_references_ncbi/ref_genome_annotation_altered.gtf > /home/osst6659/genome_analyses/analyses/10_references_ncbi/SCAFFOLD_0092/counts_92.txt
+htseq-count --format='bam' --order='name' --stranded='yes' --type='CDS' --idattr='gene_id' --samout='counts_93.sam' /home/osst6659/genome_analyses/analyses/10_references_ncbi/SCAFFOLD_0093/accepted_hits93_sorted.bam /home/osst6659/genome_analyses/analyses/10_references_ncbi/ref_genome_annotation_altered.gtf > /home/osst6659/genome_analyses/analyses/10_references_ncbi/SCAFFOLD_0093/counts_93.txt
+htseq-count --format='bam' --order='name' --stranded='yes' --type='CDS' --idattr='gene_id' --samout='counts_96.sam' /home/osst6659/genome_analyses/analyses/10_references_ncbi/SCAFFOLD_0096/accepted_hits96_sorted.bam /home/osst6659/genome_analyses/analyses/10_references_ncbi/ref_genome_annotation_altered.gtf > /home/osst6659/genome_analyses/analyses/10_references_ncbi/SCAFFOLD_0096/counts_96.txt
+htseq-count --format='bam' --order='name' --stranded='yes' --type='CDS' --idattr='gene_id' --samout='counts_97.sam' /home/osst6659/genome_analyses/analyses/10_references_ncbi/SCAFFOLD_0097/accepted_hits97_sorted.bam /home/osst6659/genome_analyses/analyses/10_references_ncbi/ref_genome_annotation_altered.gtf > /home/osst6659/genome_analyses/analyses/10_references_ncbi/SCAFFOLD_0097/counts_97.txt
